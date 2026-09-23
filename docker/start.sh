6170a7100e787dff61492f5fbb44a36e1f9b02ac
Pritam Maurya <alvin156777@gmail.com>
2026-09-23 17:51:22 +0530

    chown storage at runtime to fix permission denied on disk mount

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+﻿#!/bin/bash
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 php artisan config:clear
 php artisan config:cache
 php artisan migrate --force
